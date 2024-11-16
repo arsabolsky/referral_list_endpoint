88c19c38c914eda5e9873a2677eeac25da0e6fd2
karrittq <karter.arritt@missionary.org>
2024-11-16 15:26:21 -0500

    patch followup

--- src/main.rs
+++ src/main.rs
@@ -29,15 +29,7 @@ async fn main() {
     println!("Starting referral list program... Checking environment...");
     let save_env = env::check_vars();
     env_logger::init();
-    let mut church_client: ChurchClient = match church::ChurchClient::new(save_env).await{
-        Ok(church_client) => {
-            church_client
-        }
-        Err(error) => {
-            println!("Error! {}",error);
-            return;
-        }
-    };
+    let mut church_client: ChurchClient = church::ChurchClient::new(save_env).await.unwrap();
 
 
 
